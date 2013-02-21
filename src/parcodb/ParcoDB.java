76661768e31b0776be4ba8d9f55b34c17554ce34
Matteo De Carlo <matteo.dek@gmail.com>
2013-02-21 21:29:55 +0100

    init netbeans project

---
+++ src/parcodb/ParcoDB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcodb;
+
+/**
+ *
+ * @author matteo
+ */
+public class ParcoDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
