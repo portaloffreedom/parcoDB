c6cfe3880cdd15d15c8c62ee171711b20397fc24
Matteo De Carlo <matteo.dek@gmail.com>
2013-04-08 14:59:02 +0200

    DATABASE getSentiero
    [FEATURE]
    - aggiunta la query al Database che fornisce tutti i sentieri 
    - aggiunto toString al sentiero

--- src/parcodb/database/objects/Sentiero.java
+++ src/parcodb/database/objects/Sentiero.java
@@ -5,6 +5,7 @@
 package parcodb.database.objects;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import parcodb.database.DatabaseConnection;
 
@@ -16,13 +17,22 @@ public class Sentiero implements RemoteDBobject {
     protected Tappa[] tappe;
 
     public Sentiero(int numero_sentiero, Tappa[] tappe) throws Exception {
+        this(numero_sentiero);
         if (tappe.length < 4 || tappe.length > 10)
             throw new Exception("il sentiero deve essere composto da 4 a 10 tappe");
-        this.numero_sentiero = numero_sentiero;
-        this.lunghezza = 0.0f;
         //for(int i=0;i<tappe.length;i++) this.lunghezza = this.lunghezza + tappe[i].getLunghezza();
         this.tappe = tappe;
     }
+    
+    protected Sentiero(int numero_sentiero) {
+        this(numero_sentiero, 0.0f);
+    }
+    
+    protected Sentiero(int numero_sentiero, float lunghezza) {
+       this.numero_sentiero = numero_sentiero; 
+       this.lunghezza = lunghezza;
+    }
+        
 
     public int getNumero_sentiero() {
         return numero_sentiero;
@@ -46,6 +56,29 @@ public class Sentiero implements RemoteDBobject {
             composto.insertIntoDB(conn);
         }
         
+    }
+    
+    static public Sentiero[] getSentieri(DatabaseConnection conn) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareQueryStatement("SELECT * FROM Sentiero;");
+        
+        ResultSet result = preparedStatement.executeQuery();
+        
+        int DIM = DatabaseConnection.getResultDim(result);
+        Sentiero[] sentieri = new Sentiero[DIM];
+        int i;
+        for (i=0; result.next(); i++) {
+            sentieri[i] = new Sentiero(result.getInt(1), result.getFloat(2));
+        }
+        
+        if (i != DIM)
+            throw new SQLException("il numero di risultati di getMonti() è incongruo ("+i+','+DIM+')');
+        
+        return sentieri;
+    }
+
+    @Override
+    public String toString() {
+        return "Sentiero numero "+this.getNumero_sentiero();
     }
     
 }
